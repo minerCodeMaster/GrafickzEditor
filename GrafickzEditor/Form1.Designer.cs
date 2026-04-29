dd1e6625119fdacada0f2f0c267632a2b964343a
Kubaeej <pelajak19v@gyri.cz>
2026-04-29 15:55:16 +0200

    Barvy a velikost pera a menu

--- GrafickzEditor/Form1.Designer.cs
+++ GrafickzEditor/Form1.Designer.cs
@@ -32,17 +32,35 @@
             this.clearButton = new System.Windows.Forms.Button();
             this.statusCoords = new System.Windows.Forms.StatusStrip();
             this.statusCoordsLbl = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnRed = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.novýToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.otevřítToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uložitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.pbPlatno)).BeginInit();
             this.statusCoords.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // pbPlatno
             // 
             this.pbPlatno.BackColor = System.Drawing.Color.White;
             this.pbPlatno.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pbPlatno.Location = new System.Drawing.Point(108, 12);
+            this.pbPlatno.Location = new System.Drawing.Point(108, 27);
             this.pbPlatno.Name = "pbPlatno";
-            this.pbPlatno.Size = new System.Drawing.Size(553, 413);
+            this.pbPlatno.Size = new System.Drawing.Size(543, 398);
             this.pbPlatno.TabIndex = 0;
             this.pbPlatno.TabStop = false;
             this.pbPlatno.Paint += new System.Windows.Forms.PaintEventHandler(this.pbPlatno_Paint);
@@ -51,7 +69,7 @@
             // 
             // clearButton
             // 
-            this.clearButton.Location = new System.Drawing.Point(12, 12);
+            this.clearButton.Location = new System.Drawing.Point(12, 27);
             this.clearButton.Name = "clearButton";
             this.clearButton.Size = new System.Drawing.Size(90, 29);
             this.clearButton.TabIndex = 1;
@@ -75,20 +93,183 @@
             this.statusCoordsLbl.Size = new System.Drawing.Size(85, 17);
             this.statusCoordsLbl.Text = "Mouse coords:";
             // 
+            // btnRed
+            // 
+            this.btnRed.BackColor = System.Drawing.Color.Red;
+            this.btnRed.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnRed.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRed.Location = new System.Drawing.Point(657, 27);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(20, 20);
+            this.btnRed.TabIndex = 3;
+            this.btnRed.UseVisualStyleBackColor = false;
+            this.btnRed.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(683, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(20, 20);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(709, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(20, 20);
+            this.button2.TabIndex = 5;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Orange;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(737, 27);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(20, 20);
+            this.button3.TabIndex = 6;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Yellow;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(657, 53);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(20, 20);
+            this.button4.TabIndex = 7;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Green;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(683, 53);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(20, 20);
+            this.button5.TabIndex = 8;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Blue;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(709, 53);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(20, 20);
+            this.button6.TabIndex = 9;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Purple;
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Location = new System.Drawing.Point(737, 53);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(20, 20);
+            this.button7.TabIndex = 10;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(769, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.novýToolStripMenuItem,
+            this.otevřítToolStripMenuItem,
+            this.uložitToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(57, 20);
+            this.toolStripMenuItem1.Text = "Soubor";
+            // 
+            // novýToolStripMenuItem
+            // 
+            this.novýToolStripMenuItem.Name = "novýToolStripMenuItem";
+            this.novýToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.novýToolStripMenuItem.Text = "Nový";
+            this.novýToolStripMenuItem.Click += new System.EventHandler(this.novýToolStripMenuItem_Click);
+            // 
+            // otevřítToolStripMenuItem
+            // 
+            this.otevřítToolStripMenuItem.Name = "otevřítToolStripMenuItem";
+            this.otevřítToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.otevřítToolStripMenuItem.Text = "Otevřít";
+            this.otevřítToolStripMenuItem.Click += new System.EventHandler(this.otevřítToolStripMenuItem_Click);
+            // 
+            // uložitToolStripMenuItem
+            // 
+            this.uložitToolStripMenuItem.Name = "uložitToolStripMenuItem";
+            this.uložitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.uložitToolStripMenuItem.Text = "Uložit";
+            this.uložitToolStripMenuItem.Click += new System.EventHandler(this.uložitToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(657, 80);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(100, 20);
+            this.numericUpDown1.TabIndex = 12;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(769, 450);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnRed);
             this.Controls.Add(this.statusCoords);
+            this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.clearButton);
             this.Controls.Add(this.pbPlatno);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "FormMain";
             this.Text = "Okno is calling";
             this.Load += new System.EventHandler(this.FormMain_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pbPlatno)).EndInit();
             this.statusCoords.ResumeLayout(false);
             this.statusCoords.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,6 +281,22 @@
         private System.Windows.Forms.Button clearButton;
         private System.Windows.Forms.StatusStrip statusCoords;
         private System.Windows.Forms.ToolStripStatusLabel statusCoordsLbl;
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem otevřítToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uložitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem novýToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
     }
 }
 
--- GrafickzEditor/Form1.cs
+++ GrafickzEditor/Form1.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Windows.Forms;
+using System.Xml.Schema;
 
 namespace GrafickzEditor
 {
@@ -14,10 +18,27 @@ namespace GrafickzEditor
         bool isDrawingLine = false;
         Point startPoint;
         Point currentPoint;
+        Color dColor;
+        decimal brushSize;
+
+        public Pen getPenFromColor(Color color)
+        {
+            if (color == null) { return Pens.Black; }
+            return new Pen(color, (float)brushSize);
+        }
+
+        public Brush getBrushFromColor(Color color)
+        {
+            if (color == null) { return new SolidBrush(Color.Black); }
+            return new SolidBrush(color);
+        }
 
         public FormMain()
         {
             InitializeComponent();
+            dColor = Color.Black;
+            brushSize = 10;
+            numericUpDown1.Value = brushSize;
         }
 
         private void FormMain_Load(object sender, EventArgs e)
@@ -42,7 +63,7 @@ namespace GrafickzEditor
                 }
                 else
                 {
-                    canvasGraphics.DrawLine(Pens.Black, startPoint, e.Location);
+                    canvasGraphics.DrawLine(getPenFromColor(dColor), startPoint, e.Location);
                     isDrawingLine = false;
                     pbPlatno.Invalidate();
                 }
@@ -56,7 +77,7 @@ namespace GrafickzEditor
             if (e.Button == MouseButtons.Left)
             {
                 // Volnokresba
-                canvasGraphics.FillEllipse(Brushes.Black, e.X, e.Y, 10, 10);
+                canvasGraphics.FillEllipse(getBrushFromColor(dColor), e.X, e.Y, (float)brushSize, (float)brushSize);
                 pbPlatno.Invalidate();
             }
             else if (isDrawingLine)
@@ -72,7 +93,7 @@ namespace GrafickzEditor
             // Projekce přímky
             if (isDrawingLine)
             {
-                e.Graphics.DrawLine(Pens.Black, startPoint, currentPoint);
+                e.Graphics.DrawLine(getPenFromColor(dColor), startPoint, currentPoint);
             }
         }
 
@@ -81,6 +102,68 @@ namespace GrafickzEditor
             // Absolutní mazání
             canvasGraphics.Clear(Color.White);
             pbPlatno.Invalidate();
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            Button senderButton = (Button)sender;
+
+            dColor = senderButton.BackColor;
+        }
+
+        private void novýToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            canvasGraphics.Clear(Color.White);
+            pbPlatno.Invalidate();
+        }
+
+        private void otevřítToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "bmp files (*.bmp)|*.bmp";
+            ImageFormat format = ImageFormat.Jpeg;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //string ext = Path.GetExtension(openFileDialog1.FileName);
+                //switch (ext)
+                //{
+                //    case ".jpg":
+                //        format = ImageFormat.Jpeg;
+                //        break;
+                //    case ".png":
+                //        format = ImageFormat.Png;
+                //        break;
+                //}
+                pbPlatno.Image = new Bitmap(openFileDialog1.FileName);
+            }
+        }
+
+        private void uložitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "bmp files (*.bmp)|*.bmp";
+            ImageFormat format = ImageFormat.Jpeg;
+
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //string ext = Path.GetExtension(saveFileDialog1.FileName);
+                //switch(ext)
+                //{
+                //    case ".jpg":
+                //        format = ImageFormat.Jpeg;
+                //        break;
+                //    case ".png":
+                //        format = ImageFormat.Png;
+                //        break;
+                //}
+                pbPlatno.Image.Save(saveFileDialog1.FileName, format);
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            NumericUpDown numeric = (NumericUpDown)sender;
+
+            brushSize = numeric.Value;
         }
     }
 }
